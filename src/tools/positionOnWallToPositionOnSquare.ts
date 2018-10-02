00a888e72e1bdead8eae9d9854ed6b905cc41114
Pavol Hejny <me@pavolhejny.com>
2018-10-02 23:20:46 +0200

    positionOnWallToPositionOnSquare

--- src/tools/positionOnWallToPositionOnSquare.ts
+++ src/tools/positionOnWallToPositionOnSquare.ts
@@ -1,38 +1,28 @@
 import { ICorners } from '../model/ICorners';
 import { IVector2, IVector3 } from '../model/IVectors';
+import { cleanVectorToBabylon } from './vectors';
 
 export function positionOnWallToPositionOnSquare(
     positionOnWall: IVector3,
     corners: ICorners,
 ): IVector2 | null {
-    //console.group('convertSceneVectorToWallVector');
-    //console.log('frame.position',frame.position);
-    //console.log('frame.rotation',frame.rotation);
-    /*
-    const ray = new BABYLON.Ray(
-        cleanVectorToBabylon(frame.position),
-        frame.rotation,
-        100,
-    );
-    //console.log('ray', ray);
+
+
+    const middle = cleanVectorToBabylon(positionOnWall).subtractInPlace(cleanVectorToBabylon(corners.topLeft));
+
+
+    const left = cleanVectorToBabylon(corners.topRight).subtractInPlace(cleanVectorToBabylon(corners.topLeft));
+    const down = cleanVectorToBabylon(corners.bottomLeft).subtractInPlace(cleanVectorToBabylon(corners.topLeft));
+
+
+    const x = BABYLON.Vector3.Dot(middle,left) / left.length();
+    const y = BABYLON.Vector3.Dot(middle,down) / down.length();
 
 
 
-    const hit = wallMesh
-        .getScene()
-        .pickWithRay(ray, (mesh) => mesh === wallMesh);
-    
-    if(!hit)return null;
-
-
-    console.log(hit.hit);
-
-    //console.log('hit', hit);
-    //console.groupEnd();
-    */
 
     return {
-        x: Math.random(), //((frame.positionOnSquare.x * 100 + 250) / 600 - 0.5) * 4 + 0.5 + 0.2,
-        y: Math.random(), //(((3 - frame.positionOnSquare.y) * 100 + 200) / 600 - 0.5) * 4 + 0.5 + 0.3,
+        x, //((frame.positionOnSquare.x * 100 + 250) / 600 - 0.5) * 4 + 0.5 + 0.2,
+        y, //(((3 - frame.positionOnSquare.y) * 100 + 200) / 600 - 0.5) * 4 + 0.5 + 0.3,
     };
 }
